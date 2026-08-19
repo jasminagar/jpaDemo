c4f75d9a0b277aaff2c6c9959bfdefca50404a13
jasminagar <jasminagar21@gmail.com>
2026-08-19 10:43:11 +0200

    Initial commit

---
+++ src/main/java/app/config/EntityRegistry.java
@@ -0,0 +1,13 @@
+package app.config;
+
+import org.hibernate.cfg.Configuration;
+
+final class EntityRegistry {
+
+    private EntityRegistry() {}
+
+    static void registerEntities(Configuration configuration) {
+       // configuration.addAnnotatedClass(Study.class);
+        // TODO: Add more entities here...
+    }
+}
